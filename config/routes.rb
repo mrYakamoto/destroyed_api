250b916b640f9405d39edc33f630101cbc9b846b
Gregory Wehmeier <wehmenstein@gmail.com>
2016-05-19 16:26:15 -0500

    remove namespace for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, :defaults => {:format => :json } do
     resources :films, except: [:new, :edit]
-  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
